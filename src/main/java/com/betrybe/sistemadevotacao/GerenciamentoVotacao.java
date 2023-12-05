e387dffde21fbd069ea4311e246d17e2cc59f1f0
Victor Duque <victor.duque11@hotmail.com>
2023-12-05 11:46:30 -0300

    req 6 - implementando métodos de votar e resultadona na classe GerenciamentoVotacao

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -36,11 +36,37 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   @Override
   public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+    if (cpfsComputados.contains(cpfPessoaEleitora)) {
+      System.out.println("Pessoa eleitora já votou!");
+      return;
+    }
 
+    for (PessoaCandidata candidata : pessoasCandidatas) {
+      if (candidata.getNumero() == numeroPessoaCandidata) {
+        candidata.receberVoto();
+        cpfsComputados.add(cpfPessoaEleitora);
+        return;
+      }
+    }
+
+    System.out.println("Pessoa candidata não cadastrada!");
   }
 
   @Override
   public void mostrarResultado() {
+    int totalDeVotos = cpfsComputados.size();
 
+    if (cpfsComputados.isEmpty()) {
+      System.out.println("É preciso ter pelo menos um voto para mostrar o resultado");
+      return;
+    }
+
+    for (PessoaCandidata candidata : pessoasCandidatas) {
+      int votosPercentual = Math.round((float) totalDeVotos / candidata.getVotos());
+      System.out.println("Nome: " + candidata.getNome() + " - "
+          + candidata.getVotos() + "( " + votosPercentual + "% )");
+    }
+
+    System.out.println("Total de votos: " + totalDeVotos);
   }
 }
