cc025152231176a1955f419dd85b1feec2d53ae5
Victor Duque <victor.duque11@hotmail.com>
2023-12-05 11:07:04 -0300

    req 2 - classe PessoaCandidata

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -3,6 +3,35 @@ package com.betrybe.sistemadevotacao;
 /**
  * Classe Candidata.
  */
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  protected int numero;
+  protected int votos;
 
+  /**
+   * Construtor.
+   *
+   * @param nome    nome da pessoa candidata.
+   * @param numero  numero de voto.
+   */
+  public PessoaCandidata(String nome, int numero) {
+    setNome(nome);
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }
