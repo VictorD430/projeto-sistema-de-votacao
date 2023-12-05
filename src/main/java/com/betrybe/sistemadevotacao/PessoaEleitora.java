089ea8b5604de466d17dc053fe07f29fa4a8950a
Victor Duque <victor.duque11@hotmail.com>
2023-12-05 11:09:45 -0300

    req 3 - classe PessoaEleitora

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -3,6 +3,17 @@ package com.betrybe.sistemadevotacao;
 /**
  * Classe Eleitora.
  */
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
 
+  public PessoaEleitora() {
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
